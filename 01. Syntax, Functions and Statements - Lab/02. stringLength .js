c69f2cb4ff633af4b8dd6a1e16e5f3f4fd279588
Dimitar Mitev <d.mitev920123@gmail.com>
2022-08-23 16:13:16 +0300

    Create 02. stringLength .js

---
+++ 01. Syntax, Functions and Statements - Lab/02. stringLength .js
@@ -0,0 +1,5 @@
+function stringLength(firstArg, secondArg, thirdArg) {
+    console.log(firstArg.length + secondArg.length + thirdArg.length);
+    console.log(Math.floor((firstArg.length + secondArg.length + thirdArg.length)
+        / 3));
+}
