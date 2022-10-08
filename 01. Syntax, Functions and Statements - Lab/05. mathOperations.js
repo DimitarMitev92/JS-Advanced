c48434fa18f806e8f8815928cf74167c47071860
Dimitar Mitev <d.mitev920123@gmail.com>
2022-10-08 12:47:00 +0300

    Create 05. mathOperations.js

---
+++ 01. Syntax, Functions and Statements - Lab/05. mathOperations.js
@@ -0,0 +1,12 @@
+function mathOperations(firstNum, secondNum, operation) {
+    let result;
+    switch (operation) {
+        case '+': result = firstNum + secondNum; break;
+        case '-': result = firstNum - secondNum; break;
+        case '*': result = firstNum * secondNum; break;
+        case '/': result = firstNum / secondNum; break;
+        case '%': result = firstNum % secondNum; break;
+        case '**': result = firstNum ** secondNum; break;
+    }
+    console.log(result);
+}
