6cbe44601d369b9e02caac71876d737caaf5b478
Dimitar Mitev <d.mitev920123@gmail.com>
2022-10-10 11:38:39 +0300

    Create 10. aggregateElements.js

---
+++ 01. Syntax, Functions and Statements - Lab/10. aggregateElements.js
@@ -0,0 +1,28 @@
+function aggregateElements(arr) {
+    function sum(arr) {
+        let result = 0;
+        for (let i = 0; i < arr.length; i++) {
+            result += arr[i];
+        }
+        console.log(result);
+    }
+    sum(arr);
+
+    function sumInverse(arr) {
+        let result = 0;
+        for (let i = 0; i < arr.length; i++) {
+            result += 1 / arr[i];
+        }
+        console.log(result);
+    }
+    sumInverse(arr);
+
+    function concat(arr) {
+        let result = '';
+        for (let i = 0; i < arr.length; i++) {
+            result += arr[i].toString();
+        }
+        console.log(result);
+    }
+    concat(arr);
+}
