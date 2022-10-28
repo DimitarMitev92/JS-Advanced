944c7d0746dc2d7317261a959e83710465cb573a
Dimitar Mitev <d.mitev920123@gmail.com>
2022-10-28 10:12:39 +0300

    Create 06. storeCatalogue.js

---
+++ 06. Objects and Composition - Exercise/06. storeCatalogue.js
@@ -0,0 +1,36 @@
+function storeCatalogue(array) {
+    let resultArr = [];
+    for (let line of array) {
+        let firstChar = line[0];
+        if (resultArr.length === 0) {
+            let obj = {
+                firstChar: firstChar,
+                products: [line]
+            }
+            resultArr.push(obj)
+        } else {
+            let isFound = false;
+            for (let obj of resultArr) {
+                if (obj.firstChar === firstChar) {
+                    obj.products.push(line);
+                    isFound = true;
+                }
+            }
+            if (!isFound) {
+                let obj = {
+                    firstChar: firstChar,
+                    products: [line]
+                }
+                resultArr.push(obj);
+            }
+        }
+    }
+    let sortedArr = resultArr.sort((a, b) => a.firstChar.localeCompare(b.firstChar));
+    for (let line of sortedArr) {
+        console.log(line.firstChar);
+        for (let product of line.products.sort((a, b) => a.localeCompare(b))) {
+            product = product.replace(' : ', ': ');
+            console.log(`  ${product}`);
+        }
+    }
+}
