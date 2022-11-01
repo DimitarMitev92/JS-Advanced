23eb8949ed821a835760d759bb125fc865f1cdf1
Dimitar Mitev <d.mitev920123@gmail.com>
2022-11-01 09:32:14 +0200

    Create 03. sumNumbers.js

---
+++ 07. DOM Introduction - Lab/03. sumNumbers.js
@@ -0,0 +1,5 @@
+function calc() {
+  const firstNum = Number(document.querySelector('#num1').value);
+  const secondNum = Number(document.querySelector('#num2').value);
+  document.querySelector('#sum').value = firstNum + secondNum;
+};
