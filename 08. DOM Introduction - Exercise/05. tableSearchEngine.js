718b43672f8a8d13a7bddfe6d5278817d0d06592
Dimitar Mitev <d.mitev920123@gmail.com>
2022-11-05 12:21:06 +0200

    Create 05. tableSearchEngine.js

---
+++ 08. DOM Introduction - Exercise/05. tableSearchEngine.js
@@ -0,0 +1,15 @@
+function solve() {
+  document.querySelector('#searchBtn').addEventListener('click', onClick);
+
+  const rows = document.querySelectorAll('tbody tr');
+  const input = document.getElementById('searchField');
+  function onClick() {
+    for (let row of rows) {
+      row.classList.remove('select');
+      if (row.innerHTML.includes(input.value)) {
+        row.className = 'select'
+      }
+    }
+    input.value = '';
+  }
+}
